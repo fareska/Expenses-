b82ba9af99a9352339129c04972c1b57ad8f0256
fareska <fares.xb@gmail.com>
2020-11-30 17:01:10 +0200

    setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const api = require('./server/routes/api')
+const Expense = require('./server/model/Expense')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/expensesDB', {useNewUrlParser: true})
+
+const data = require(`./data`)
+data.forEach(d => {
+    let e = new Expense ({
+        name:  d.item,
+        amount: d.amount,
+        date:  d.date,
+        group: d.group
+    })
+    e.save()
+});
+
+app.use('/', api)
+
+
+const port = 4100
+app.listen(port, function () {
+    console.log(`Running on port ${port}`)
+})
